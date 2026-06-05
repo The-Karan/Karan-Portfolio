6d9f2920c95510375030bdc3eb47ec6fa91a2d31
The-Karan <KaranSharma.ab9904@gmail.com>
2026-06-05 18:40:32 +0530

    Add portfolio website

--- vite.config.js
+++ vite.config.js
@@ -6,31 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 650,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (
-              id.includes("three") ||
-              id.includes("@react-three") ||
-              id.includes("@pmndrs") ||
-              id.includes("maath")
-            ) {
-              return "three-vendor";
-            }
-
-            if (id.includes("framer-motion")) {
-              return "motion-vendor";
-            }
-
-            if (id.includes("react")) {
-              return "react-vendor";
-            }
-
-            return "vendor";
-          }
-        },
-      },
-    },
   },
 });
